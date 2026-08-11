fbd9473b07b0c61541b5628240a3c4ae953ea046
sujeet kumar <sujeetkumar2010@live.com>
2026-08-11 11:22:52 +0530

    only api key here

--- ai_agent.py
+++ ai_agent.py
@@ -1,1 +1,8 @@
-print("sujeet kumar")
+from dotenv import load_dotenv
+load_dotenv()  # Load environment variables from .env file
+import os
+
+#Step1: Setup API Keys for Groq, OpenAI and Tavily
+GROQ_API_KEY= os.getenv("GROQ_API_KEY")
+TAVILY_API_KEY=os.environ.get("TAVILY_API_KEY")
+OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
