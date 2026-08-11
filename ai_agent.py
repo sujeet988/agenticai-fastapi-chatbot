9d24a4a0f2aac5af0fb8cc433ffbcacac4171d3c
sujeet kumar <sujeetkumar2010@live.com>
2026-08-11 11:27:50 +0530

    new key added

--- ai_agent.py
+++ ai_agent.py
@@ -6,3 +6,12 @@ import os
 GROQ_API_KEY= os.getenv("GROQ_API_KEY")
 TAVILY_API_KEY=os.environ.get("TAVILY_API_KEY")
 OPENAI_API_KEY=os.environ.get("OPENAI_API_KEY")
+
+print("GROQ_API_KEY:", GROQ_API_KEY)
+print("TAVILY_API_KEY:", TAVILY_API_KEY)
+print("OPENAI_API_KEY:", OPENAI_API_KEY)    
+
+#Step2: Setup LLM & Tools
+from langchain_groq import ChatGroq
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
