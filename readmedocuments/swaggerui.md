a04e8eeff4cbccdd49fac3f1ae63a1fbe1e89945
sujeet kumar <sujeetkumar2010@live.com>
2026-08-15 11:43:45 +0530

    streamlit changes

--- readmedocuments/swaggerui.md
+++ readmedocuments/swaggerui.md
@@ -4,3 +4,6 @@ Chat API	http://127.0.0.1:9999/chat
 Swagger UI	http://127.0.0.1:9999/docs
 ReDoc	http://127.0.0.1:9999/redoc
 OpenAPI JSON	http://127.0.0.1:9999/openapi.json
+
+## Streamlit  run 
+-streamlit run frontend.py
---
+++ src/frontend.py
@@ -0,0 +1,12 @@
+## Phase3–Setup Frontend
+##1. Setup UI with streamlit (model provider, model, system prompt, query)
+import streamlit as st
+
+st.set_page_config(page_title="Chat with LLM", layout="wide")
+st.title("Ai Chat bot agent")
+st.write("create and interact with your own AI chat bot agent")
+
+
+
+
+##2. Connect with backend via URL
